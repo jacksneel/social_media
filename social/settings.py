b934cf1516899dbdc9d150a11a7cbef984a305ea
jacksneel <99187556+jacksneel@users.noreply.github.com>
2026-02-25 21:33:14 +0530

    Update settings.py

--- social/settings.py
+++ social/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-#h_vp-^3w*^%2vi&3w)712s@cp86g1h#drj(ow_+u%#wr3v1t#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['3.88.63.225', 'localhost', '0.0.0.0', 172.17.0.2, 3.83.187.233]
+ALLOWED_HOSTS = ['3.88.63.225', 'localhost', '0.0.0.0', '172.17.0.2', '3.86.242.179', 172.31.87.193 ]
 
 
 # Application definition
@@ -199,6 +199,7 @@ SOCIALACCOUNT_PROVIDERS = {
        
     }
 }
+
 
 
 
